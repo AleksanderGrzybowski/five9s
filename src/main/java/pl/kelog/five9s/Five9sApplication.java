f35493a09a054ab90d86bf5a3de33313262fa09d
Aleksander Grzybowski <kelog@orange.pl>
2018-06-26 19:00:45 +0200

    Empty app

---
+++ src/main/java/pl/kelog/five9s/Five9sApplication.java
@@ -0,0 +1,12 @@
+package pl.kelog.five9s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Five9sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Five9sApplication.class, args);
+	}
+}
