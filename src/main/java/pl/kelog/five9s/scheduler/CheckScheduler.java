bb82b80161e131d7acdb63b04cd1a113bf8aa88c
Aleksander Grzybowski <kelog@orange.pl>
2018-07-03 18:46:29 +0200

    Add SSH checks to main skeleton

--- src/main/java/pl/kelog/five9s/scheduler/CheckScheduler.java
+++ src/main/java/pl/kelog/five9s/scheduler/CheckScheduler.java
@@ -6,10 +6,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import pl.kelog.five9s.checks.CheckStatus;
 import pl.kelog.five9s.checks.HttpChecker;
+import pl.kelog.five9s.checks.SshChecker;
 import pl.kelog.five9s.db.PerformedCheckInfo;
 import pl.kelog.five9s.db.PerformedCheckRepository;
 import pl.kelog.five9s.utils.DateService;
 import pl.kelog.five9s.yamlimport.ServiceDefinition;
+import pl.kelog.five9s.yamlimport.ServiceDefinition.CheckDefinition;
 import pl.kelog.five9s.yamlimport.ServiceDefinitionRepository;
 
 @Service
@@ -20,6 +22,7 @@ public class CheckScheduler {
     private final ServiceDefinitionRepository serviceDefinitionRepository;
     private final PerformedCheckRepository performedCheckRepository;
     private final HttpChecker httpChecker;
+    private final SshChecker sshChecker;
     private final DateService dateService;
     
     @Scheduled(fixedRate = 10000, initialDelay = 1000)
@@ -33,17 +36,29 @@ public class CheckScheduler {
     
     private void performCheck(ServiceDefinition serviceDefinition) {
         log.info("Running " + serviceDefinition.name + "(" + serviceDefinition.description + ")...");
+        CheckDefinition checkDefinition = serviceDefinition.checkDefinition;
         
-        if (serviceDefinition.checkDefinition.type == ServiceDefinition.CheckType.HTTP) {
-            CheckStatus status = httpChecker.perform(serviceDefinition.checkDefinition.url, 10000);
-            log.info("Status of " + serviceDefinition.name + " -> " + status + ", saving.");
+        CheckStatus status;
+        if (checkDefinition.type == ServiceDefinition.CheckType.HTTP) {
+            status = httpChecker.perform(checkDefinition.url, 10000);
             
-            PerformedCheckInfo info = new PerformedCheckInfo();
-            info.setName(serviceDefinition.name);
-            info.setTimestamp(dateService.now());
-            info.setStatus(status.status);
-            info.setLog(status.log);
-            performedCheckRepository.save(info);
+        } else if (checkDefinition.type == ServiceDefinition.CheckType.SSH) {
+            status = sshChecker.perform(
+                    checkDefinition.host,
+                    checkDefinition.user,
+                    checkDefinition.keyPath,
+                    checkDefinition.command
+            );
+        } else {
+            throw new AssertionError("Unknown check type " + checkDefinition.type);
         }
+        
+        log.info("Status of " + serviceDefinition.name + " -> " + status + ", saving.");
+        PerformedCheckInfo info = new PerformedCheckInfo();
+        info.setName(serviceDefinition.name);
+        info.setTimestamp(dateService.now());
+        info.setStatus(status.status);
+        info.setLog(status.log);
+        performedCheckRepository.save(info);
     }
 }
