821f6aaee9a133da3f903f03355a5f8700fe8bea
Aleksander Grzybowski <kelog@orange.pl>
2018-07-06 19:33:24 +0200

    Add request logging

---
+++ src/main/java/pl/kelog/five9s/config/RequestLoggingConfig.java
@@ -0,0 +1,18 @@
+package pl.kelog.five9s.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+    
+    @Bean
+    public CommonsRequestLoggingFilter requestLogger() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeClientInfo(true);
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        return filter;
+    }
+}
